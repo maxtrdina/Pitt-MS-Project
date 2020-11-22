858497317fcafce6f4d96b5d1dd8cf79de8d2dea
Ismael Alonso <ismaha91@gmail.com>
2020-11-22 01:29:19 -0500

    Build agent against spines

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-gcc agent.c map.c agent_control.c agent_data.c -o mininet/agent -lpthread -std=gnu99
+# gcc agent.c map.c agent_control.c agent_data.c -o mininet/agent -lpthread -std=gnu99
 gcc manager.c network_manager.c topology_manager.c net_util.c map.c -o mininet/manager -lpthread -std=gnu99
 gcc client.c net_util.c -o mininet/client -lpthread -std=gnu99
+
+gcc -g -O2 -Wall -D_REENTRANT -DNDEBUG -fPIC -c -o tmp/map.o map.c
+gcc -g -O2 -Wall -D_REENTRANT -DNDEBUG -fPIC -c -o tmp/agent_control.o agent_control.c
+gcc -g -O2 -Wall -D_REENTRANT -DNDEBUG -fPIC -c -o tmp/agent_data.o agent_data.c
+
+gcc -g -O2 -Wall -D_REENTRANT -DNDEBUG -fPIC  -Ispines/stdutil/include -Ispines/libspread-util/include -Ispines/libspines -Ispines/daemon -Ispines/stdutil/include -Ispines/libspread-util/include -Ispines/libspines -Ispines/daemon -c -o tmp/agent.o agent.c
+gcc -Lspines/stdutil/lib -Lspines/libspread-util/lib -Lspines/libspines -Lspines/stdutil/lib -Lspines/libspread-util/lib -Lspines/libspines -o mininet/agent tmp/agent.o tmp/map.o tmp/agent_control.o tmp/agent_data.o spines/stdutil/lib/libstdutil.a spines/libspread-util/lib/libspread-util.a spines/libspines/libspines.a -ldl -lrt -lm -lnsl -lpthread -lcrypto
